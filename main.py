24c5bc681568231138a66bdba3ce9ef322db762f
fkdvlphm <96056768+fkdvlphm@users.noreply.github.com>
2021-12-17 19:35:35 +0330

    Update main.py
    load contacts must be invoked before gui or console starting point

--- main.py
+++ main.py
@@ -3,10 +3,10 @@ import Gui
 import Utils
 import Console
 
+Utils.load_contacts()
 if len(sys.argv) > 1 and sys.argv[1] == "-cli":
     root = Console.Console()
 else:
     root = Gui.Gui()
-Utils.load_contacts()
 root.mainloop()
 Utils.dump_contacts() # when program destroyed
